52a9b7ad3b7351d0cc4b8945542c0ea95a4255e0
blaktai <55172595+blaktai@users.noreply.github.com>
2020-07-04 16:45:40 -0400

    Fixed stack bug in Linked List implementation

--- data_structures/swift/datastructures/Stack.swift
+++ data_structures/swift/datastructures/Stack.swift
@@ -6,24 +6,26 @@ struct StackLL<T:Equatable> {
   public mutating func push(value:T){
     items.insert(value:value)
   }
-    
+  
   @discardableResult
-  public mutating func remove(value:T) -> Bool {
-    return items.removeLast()
+  public mutating func pop() -> T? {
+    let last = items.last
+    items.removeLast()
+    return last?.value
   }
 }
 
 struct StackArray<T> {
-    private var items = [T]()
-    
-    public var count: Int { return items.count }
-    
-    mutating public func pop() -> T? {
-        guard items.count > 0 else { return nil }
-        return items.removeLast()
-    }
-    
-    mutating public func push(value:T){
-        items.append(value)
-    }
+  private var items = [T]()
+  
+  public var count: Int { return items.count }
+  
+  mutating public func pop() -> T? {
+    guard items.count > 0 else { return nil }
+    return items.removeLast()
+  }
+  
+  mutating public func push(value:T){
+    items.append(value)
+  }
 }
