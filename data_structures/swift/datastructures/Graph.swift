d27707fce580dc1f607189bc46c16627fa018bd4
blaktai <55172595+blaktai@users.noreply.github.com>
2020-07-08 23:59:04 -0400

    Edge added

--- data_structures/swift/datastructures/Graph.swift
+++ data_structures/swift/datastructures/Graph.swift
@@ -4,3 +4,9 @@ struct Vertex<T:Equatable & Hashable>: Equatable, Hashable {
   public let data: T
   public let index:Int
 }
+
+struct Edge<T:Equatable & Hashable>: Equatable, Hashable {
+  public let from: Vertex<T>
+  public let to: Vertex<T>
+  public var weight: Double?
+}
