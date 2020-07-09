8d4155b03ed16610581339cbafd60f900e60ac3e
blaktai <55172595+blaktai@users.noreply.github.com>
2020-07-08 23:59:59 -0400

    Graph Protocol

--- data_structures/swift/datastructures/Graph.swift
+++ data_structures/swift/datastructures/Graph.swift
@@ -10,3 +10,14 @@ struct Edge<T:Equatable & Hashable>: Equatable, Hashable {
   public let to: Vertex<T>
   public var weight: Double?
 }
+
+protocol Graph {
+  associatedtype Element: Hashable & Equatable
+  var edges: [Edge<Element>] { get }
+  var vertices: [Vertex<Element>] { get }
+  func addUndirectedEdge(_ from: Vertex<Element>, _ to: Vertex<Element>, weight: Double?)
+  func addDirectedEdge(_ from: Vertex<Element>, _ to: Vertex<Element>, weight: Double?)
+  func weightFrom(edge: Edge<Element>) -> Double?
+  func edgesFrom(vertex: Vertex<Element>) -> [Edge<Element>]
+  func createVertex(data:Element) -> Vertex<Element>
+}
