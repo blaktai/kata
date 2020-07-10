3000553a01b3b026c08fa472820d6a8596a0ec6d
blaktai <55172595+blaktai@users.noreply.github.com>
2020-07-09 23:06:46 -0400

    Added Trie container

--- data_structures/swift/datastructures/Trie.swift
+++ data_structures/swift/datastructures/Trie.swift
@@ -1,17 +1,26 @@
 import Foundation
 
-class Node<T:Hashable> {
-  var value: T
-  weak var parent: Node?
-  var children: [T:Node] = [:]
-  
-  init(value:T,parent:Node? = nil){
-    self.value = value
-    self.parent = parent
+class Trie {
+  fileprivate var root: Node<Character>
+  init(_ char:Character){
+    self.root = Node(value: char)
   }
-  
-  public func add(value:T){
-    guard children[value] == nil else { return }
-    children[value] = Node(value: value, parent: self)
+}
+
+extension Trie {
+  class Node<T:Hashable> {
+    var value: T
+    weak var parent: Node?
+    var children: [T:Node] = [:]
+    
+    init(value:T,parent:Node? = nil){
+      self.value = value
+      self.parent = parent
+    }
+    
+    public func add(value:T){
+      guard children[value] == nil else { return }
+      children[value] = Node(value: value, parent: self)
+    }
   }
 }
